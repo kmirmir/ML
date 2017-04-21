4b98c24c4278b83c997c8c6c328ed7c231c40093
b.kihoon <masinogns@gmail.com>
2017-04-21 16:40:32 +0900

    1 clear

--- examination/java-framework-class-miexam-master/test/kr/ac/jejunu/ProductDaoTest.java
+++ examination/java-framework-class-miexam-master/test/kr/ac/jejunu/ProductDaoTest.java
@@ -15,7 +15,7 @@ public class ProductDaoTest {
         String title = "제주감귤";
         Integer price = 15000;
 
-        ProductDao productDao = new ProductDao();
+        ProductDao productDao = new JejuProductDao();
         Product product = productDao.get(id);
         assertThat(id, is(product.getId()));
         assertThat(title, is(product.getTitle()));
@@ -33,7 +33,7 @@ public class ProductDaoTest {
         product.setTitle(title);
         product.setPrice(price);
 
-        ProductDao productDao = new ProductDao();
+        ProductDao productDao = new JejuProductDao();
         productDao.add(product);
         Product addProduct = productDao.get(id);
         assertThat(id, is(addProduct.getId()));
