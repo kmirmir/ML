3f0e077d98d0395f2d8ae6893bba220e551f6791
b.kihoon <masinogns@gmail.com>
2017-06-30 11:28:20 +0900

    ToyLSTM에서 raw data를 원하는 형태로 변환함.

--- RNN/ToyLSTM.py
+++ RNN/ToyLSTM.py
@@ -1,38 +1,30 @@
 import csv
+import numpy as np
 
-
-
-def getInfoFromCsv(csvName):
-    list = []
-
-    with open(csvName, newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            list.append(row)
-
-    return list
-
-
-list = getInfoFromCsv("data.csv")
-# print(list)
-result = []
-# 0일에서 24시간이 1일이니까 x일 동안
-for i in range(0, 24*5):
-    # print(list[i])
-    result.append(list[i])
-
-print("=======")
-print("=======")
-print("=======")
-print(result[:])
-print(result[:,[-1]])
-
-print("=======")
-print("=======")
-print("=======")
-
-dataX = []
-dataY = []
-
+list = np.loadtxt("output.csv", delimiter=',')
+# result = []
+#
+# for i in range(len(list)):
+#     result.append(list[i].replace("\"", ""))
+#
+# print(result)
 # for i in range(len(result)):
-    # print(result[i])
+#     print(result[i])
+# #
+# f = open('output.csv', 'w', encoding='utf-8', newline='')
+# wr = csv.writer(f)
+# for i in range(len(result)):
+#     wr.writerow(result[i])
+# f.close()
+
+dataX = list
+dataY = list[:,[-1]]
+
+for i in range(len(dataX)):
+    print(dataX[i])
+
+print("==========")
+print("==========")
+
+for i in range(len(dataY)):
+    print(dataY[i])
