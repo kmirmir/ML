0457ca7f349c98f5c294d1a76a9236d12e960662
백기훈 <masinogns@gmail.com>
2017-07-13 11:22:06 +0900

    loss function이랑 predic plot 자동으로 png로 저장되게 만듬. *.png는 gitignore!

--- RNN/MyLib/executeRNN.py
+++ RNN/MyLib/executeRNN.py
@@ -1,6 +1,8 @@
 from RNN.MyLib.lib_rnn import RNNLibrary
 from RNN.MyLib.lib_dataset import Database
 
+
+
 class DB(Database):
     def init_dataset(self):
         self.nomalization()
@@ -9,9 +11,9 @@ class RNN(RNNLibrary):
     def init_rnn_library(self):
         self.setParams(seq_length=7, input_dim=7, output_dim=1)
         self.setPlaceholder(seq_length=rnn.seq_length, input_dim=rnn.input_dim)
-        self.setHypothesis(hidden_dim=10, layer=4)
+        self.setHypothesis(hidden_dim=hidden_dim, layer=layer)
         self.setCostfunction()
-        self.setOptimizer(learning_rate=0.01)
+        self.setOptimizer(learning_rate=learning_rate)
 
 # 일사량은 단위면적이 단위시간에 받는 일사에너지의 양으로 정의되며, 순간 복사량과 정해진 시간동안 단위면적이 받는 총열량인 복사량으로 표현됩니다.
 # 날짜,시간,수평일사량,경사일사량,외기온도,모듈온도,VCB 출력,ACB 출력,인버터 츨력
@@ -19,11 +21,20 @@ class RNN(RNNLibrary):
 # ACB = 기중 차단기 Air Circuit Breacker
 # 출력은 인버터 출력으로 함
 
+hidden_dim = 10
+layer = 4
+learning_rate = 0.01
+
 if __name__ == '__main__':
+    path = '/Users/masinogns/PycharmProjects/ML/RNN/MyLib'
+    load_file_name = '/dataToFourHour.csv'
+    save_error_file_name = '/show/'+'error'+'lr'+str(learning_rate)+'layer'+str(layer)+'hidden'+str(hidden_dim)+'.png'
+    save_predict_file_name = '/show/'+'predict'+'lr'+str(learning_rate)+'layer'+str(layer)+'hidden'+str(hidden_dim)+'.png'
+
     db = DB()
-    db.load('/Users/masinogns/PycharmProjects/ML/RNN/MyLib/dataToFourHour.csv', seq_length=7)
+    db.load(path+load_file_name, seq_length=7)
 
     rnn = RNN()
-    rnn.learning(db.trainX, db.trainY, loop=100, total_epoch=3, check_step=100)
-    rnn.showErrors()
-    rnn.prediction(db.testX, db.testY)
+    rnn.learning(db.trainX, db.trainY, loop=100, total_epoch=1, check_step=100)
+    rnn.showErrors(error_save_filename=path+save_error_file_name)
+    rnn.prediction(db.testX, db.testY, predict_save_filename=path+save_predict_file_name)
--- RNN/MyLib/lib_rnn.py
+++ RNN/MyLib/lib_rnn.py
@@ -37,31 +37,6 @@ class RNNLibrary:
         self.X = tf.placeholder(tf.float32, [None, seq_length, input_dim])
         self.Y = tf.placeholder(tf.float32, [None, 1])
 
-    def learning(self, trainX=None, trainY=None, loop=None, total_epoch = 1, check_step=0):
-        tf.set_random_seed(777)  # reproducibility
-
-        self.init_rnn_library()
-
-        self.sess = tf.Session()
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
-
-        for epoch in range(total_epoch):
-            total_cost = 0
-
-            for i in range(loop):
-                _, step_loss = self.sess.run([self.train, self.cost], feed_dict={self.X: trainX, self.Y: trainY})
-                print("[step: {}] loss: {}".format(i, step_loss))
-                self.errors.append(step_loss)
-                total_cost += step_loss
-                self.epoch_cost.append(total_cost)
-
-                if i % check_step == 0:
-                    sys.stdout.write('.')
-                    sys.stdout.flush()
-
-        print('\nDone!\n')
-
     def setHypothesis(self, hidden_dim, layer=1, isDropout=False, dropout_value=1):
         def lstm_cell():
             cell = tf.contrib.rnn.BasicLSTMCell(
@@ -77,36 +52,65 @@ class RNNLibrary:
 
         self.hypothesis = tf.contrib.layers.fully_connected(outputs[:, -1], self.output_dim, activation_fn=None)
 
-    def prediction(self, testX, testY):
-        targets = tf.placeholder(tf.float32, [None, 1])
-        predictions = tf.placeholder(tf.float32, [None, 1])
-        rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
-
-        test_predict = self.sess.run(self.hypothesis, feed_dict={self.X: testX})
-        rmse_val = self.sess.run(rmse, feed_dict={targets: testY, predictions: test_predict})
-
-        print("RMSE: {}".format(rmse_val))
-        print("RMSE: {:.2%}".format(rmse_val))
-
-        plot.plot(testY)
-        plot.plot(test_predict)
-        plot.xlabel("Test Size (blue is TestY, orange is Predict")
-        plot.ylabel("Invertor Output")
-        plot.show()
-
     def setCostfunction(self):
         self.cost = tf.reduce_sum(tf.square(self.hypothesis - self.Y))  # sum of the squares
+
 
     def setOptimizer(self, learning_rate):
         self.optimizer = tf.train.AdamOptimizer(learning_rate)
         self.train = self.optimizer.minimize(self.cost)
 
-    def showErrors(self):
-        attr = 'o-'  # 선 속성
+    def showErrors(self, error_save_filename):
+        # attr = 'o-'  # 선 속성
+        fig = plot.figure()
         x_label = ''
         y_label = ''
 
-        plot.plot(self.errors, attr)
+        plot.plot(self.errors)
         plot.xlabel(x_label)
         plot.ylabel(y_label)
+        plot.savefig(error_save_filename)
         plot.show()
+        plot.close()
+
+    def learning(self, trainX=None, trainY=None, loop=None, total_epoch = 1, check_step=0):
+
+        self.init_rnn_library()
+
+        self.sess = tf.Session()
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+        for epoch in range(total_epoch):
+            total_cost = 0
+
+            for i in range(loop):
+                self.sess.run(self.train, feed_dict={self.X: trainX, self.Y: trainY})
+                loss = self.sess.run(self.cost, feed_dict={self.X: trainX, self.Y: trainY})
+                # _, step_loss = self.sess.run([self.train, self.cost], feed_dict={self.X: trainX, self.Y: trainY})
+                # print("[step: {}] loss: {}".format(i, step_loss))
+                self.errors.append(loss)
+                # total_cost += step_loss
+                self.epoch_cost.append(total_cost)
+
+                if i % check_step == 0:
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
+
+        print('\nDone!\n')
+
+    def prediction(self, testX, testY, predict_save_filename):
+        test_predict = self.sess.run(self.hypothesis, feed_dict={self.X: testX})
+        rmse = tf.sqrt(tf.reduce_mean(tf.square(testY - test_predict)))  # 차의 제곱의 평균의 sqrt
+
+        print("RMSE: {:.2%}".format(self.sess.run(rmse)))
+        print("RMSE: ", self.sess.run(rmse))
+
+        fig = plot.figure()
+        plot.plot(testY, linestyle='-')
+        plot.plot(test_predict, linestyle='--')
+        plot.xlabel("Test Size (blue is TestY, orange is Predict")
+        plot.ylabel("Invertor Output")
+        plot.savefig(predict_save_filename)
+        plot.show()
+        plot.close()
