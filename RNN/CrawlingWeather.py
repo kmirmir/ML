e3a5244746991e4e92fd8044cad467dd3f898919
백기훈 <masinogns@gmail.com>
2017-07-19 11:33:22 +0900

    crawling 기상청 날씨 정보 추가적인 요구 사항 - 윤달을 추가적으로 넣어서 기존에 있는 데이터와 합칠 때 문제가 없게 만든다. - 날씨 feature를 확인 할 수 있게 한다 - 날씨 뿐만이 아니라 평균 풍속, 습도, 평균, 최저, 최고 기온 등 확장할 수 있게 만든다면 더욱 좋을 것 같다.

--- RNN/CrawlingWeather.py
+++ RNN/CrawlingWeather.py
@@ -1,34 +1,93 @@
 from urllib.request import urlopen
-
-import sys
 from bs4 import BeautifulSoup
 
-#         sys.stdout.write(result[i].get_text() + ", ")
-#         sys.stdout.flush()
 
-# wantYear = "2012"
-url = "http://www.kma.go.kr/weather/climate/past_table.jsp?stn=184&yy=2012&obs=90&x=29&y=14"
-print(url)
-html = urlopen(url)
-bsObject = BeautifulSoup(html.read(), "html.parser")
-table = bsObject.find("table", {"class": "table_develop"})
-cells = []
+def urlMaker(year):
+    '''
+    url에는 요소가 3개가 있다
+    지점 연도 요소
+    지점은 지역을 정할 수 있다
+    연도는 연도별 자료
+    요소는 날씨 강수량 평균풍속 ... 이 있다
 
-for row in table.findAll("tr"):
-    cells.append(row.findAll("td"))
+    stn 184는 제주 지역을 의미한다
+    yy 2012는 2012년도를 의미한다
+    obs=90&x=28&y=10은 날씨 요소를 obs=07&x=21&y=8은 평균기온을 의미한다
 
-'''
- if yy == 2012, 2012년도의 날씨들을 가져오는 것
- :param a 월을 나타낸다
- :param i 일을 나타낸다
- a 월 i 일 ok?
-'''
-for a in range(1,13):
-    print("======"+str(a)+"월======")
+    :return: url
+    '''
+    domainName = "http://www.kma.go.kr/weather/climate/past_table.jsp?"
+    year = "yy="+str(year)
+    identify = "stn=184" \
+               "&" \
+               + year +\
+               "&" \
+               "obs=90&x=29&y=14"
 
-    for i in range(1, 32):
-        # 1 월달 날씨들
-        if ord(cells[1][6].get_text()) == 160:
-            print("맑음")
-        else:
-            print(str(i)+"일 :"+cells[i][a].get_text())
+    url = domainName + identify
+    # print("url making : " + url)
+
+    return url
+
+def crawlingWeather(url):
+    # print("result of url maker : " + url)
+    html = urlopen(url)
+    bsObject = BeautifulSoup(html.read(), "html.parser")
+    table = bsObject.find("table", {"class": "table_develop"})
+    cells = []
+    for row in table.findAll("tr"):
+        cells.append(row.findAll("td"))
+
+    return cells
+
+
+def printResultOfWeather(cells):
+    '''
+     if yy == 2012, 2012년도의 날씨들을 가져오는 것
+     :param a 월을 나타낸다
+     :param i 일을 나타낸다
+     a 월 i 일 ok?
+    '''
+    for a in range(1, 13):  # 1월부터 12월까지 반복문을 실행한다
+        print("======" + str(a) + "월======")
+
+        for i in range(1, 32):  # 1일부터 31일까지 반복문을 실행한다
+            # 1 월달 날씨들
+            # print(cells[1][6].get_text())
+            compareString = cells[i][a].get_text()
+            if ord(compareString[0]) != 160:
+                print(str(i) + "일 :" + cells[i][a].get_text())
+            else:
+                print(str(i) + "일 :맑음")
+
+def findFeatureOfWeather(cells):
+    feature = []
+
+    for a in range(1, 13):  # 1월부터 12월까지 반복문을 실행한다
+        # print("======" + str(a) + "월======")
+        for i in range(1, 32):  # 1일부터 31일까지 반복문을 실행한다
+            get_text = cells[i][a].get_text()
+            # print(str(i) + "일 :" + get_text)
+
+            for i in range(len(feature)):
+                if feature[i] != get_text:
+                    feature.append(get_text)
+
+    print("finish find feature weather")
+    for i in range(len(feature)):
+        print(feature[i])
+
+    return feature
+
+def printResultOfFind(feature):
+    for i in range(len(feature)):
+        print(feature[i])
+
+url = urlMaker(year=2012)
+cells = crawlingWeather(url)
+printResultOfWeather(cells)
+
+# feature = findFeatureOfWeather(cells)
+# printResultOfFind(feature)
+
+
--- RNN/dataToImage.py
+++ RNN/dataToImage.py
@@ -14,23 +14,31 @@ import matplotlib.pyplot as plot
 # print("========")
 # print(df.info())
 
+path = '/Users/masinogns/PycharmProjects/ML/RNN/MyLib/'
+data1_path = 'dataToDay'
+data2_path = 'dataToFourHour'
+data3_path = 'dataToHour'
+data1 = np.loadtxt(path+data1_path+'.csv', delimiter=',')
+data2 = np.loadtxt(path+data2_path+'.csv', delimiter=',')
+data3 = np.loadtxt(path+data3_path+'.csv', delimiter=',')
 
-data1 = np.loadtxt('/Users/masinogns/PycharmProjects/ML/RNN/dataToDay.csv', delimiter=',')
-data2 = np.loadtxt('/Users/masinogns/PycharmProjects/ML/RNN/MyLib/dataToDay.csv', delimiter=',')
-data3 = np.loadtxt('/Users/masinogns/PycharmProjects/ML/RNN/MyLib/dataToHour.csv', delimiter=',')
-
-x1 = data1[:, [3]]
-x2 = data2[:, [3]]
-x3 = data3[:, [3]]
+# csv에서 데이터 몇번째 거를 가지고 표에 나타낼지 정하는 부분
+# x1 = data1[1:100, [3]]
+# x2 = data2[1:100, [3]]
+x3 = data3[1:150, [3]]
+y3 = data3[1:150, [5]]
 
 
-def showImage(x, x_label):
-    attr = 'o-'  # 선 속성
-    plot.plot(x, attr)
+def showImage(x, x_label, save_file_name):
+    # attr = 'o-'  # 선 속성
+    plot.plot(x)
     plot.xlabel(x_label)
+    plot.savefig(save_file_name+'.png')
+    # plot.close('all')
     plot.show()
 
 
-showImage(x1, "4 binding")
-# showImage(x2, "24 binding")
-# showImage(x3, "all 24hour")
+# showImage(x1, "24 binding", data1_path)
+# showImage(x2, "4 binding", data2_path)
+showImage(x3, "all 24hour", data3_path+'일사량')
+showImage(y3, "all 24hour", data3_path+'온도')
